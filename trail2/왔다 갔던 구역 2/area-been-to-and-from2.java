4834b7b8fec5a989bd20012cee31725b9d681cac
Jinu Park <136476414+EddieDawn@users.noreply.github.com>
2026-09-07 17:31:38 +0900

    [Passed] 왔다 갔던 구역 2 (91ms, 9MB)

--- trail2/왔다 갔던 구역 2/area-been-to-and-from2.java
+++ trail2/왔다 갔던 구역 2/area-been-to-and-from2.java
@@ -2,6 +2,7 @@ import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -13,6 +14,7 @@ public class Main {
 
         int idx = 1000;
         int[] arr = new int[2001];
+        Arrays.fill(arr, 0);
         int N = Integer.parseInt(br.readLine());
 
         for (int i = 0; i < N; i++) {
@@ -20,14 +22,16 @@ public class Main {
             int num = Integer.parseInt(st.nextToken());
             String dir = st.nextToken();
             int dirPM = dir.equals("R") ? 1 : -1;
+//            System.out.println(dirPM);
             for (int j = 0; j < num; j++) {
                 if (dirPM > 0) {
                     arr[idx]++;
                     idx++;
                 } else {
-                    arr[idx]++;
+                    arr[idx - 1]++;
                     idx--;
                 }
+//                System.out.println(Arrays.toString(arr) + " idx: " + idx);
             }
 
         }
