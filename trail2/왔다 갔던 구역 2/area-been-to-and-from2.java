a785c33f35bb1582e50d76fa272e39c22fcd7cb3
Jinu Park <136476414+EddieDawn@users.noreply.github.com>
2026-09-07 17:25:21 +0900

    [Wrong Answer] 왔다 갔던 구역 2 (98ms, 9MB)

---
+++ trail2/왔다 갔던 구역 2/area-been-to-and-from2.java
@@ -0,0 +1,45 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+
+        int idx = 1000;
+        int[] arr = new int[2001];
+        int N = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            int num = Integer.parseInt(st.nextToken());
+            String dir = st.nextToken();
+            int dirPM = dir.equals("R") ? 1 : -1;
+            for (int j = 0; j < num; j++) {
+                if (dirPM > 0) {
+                    arr[idx]++;
+                    idx++;
+                } else {
+                    arr[idx]++;
+                    idx--;
+                }
+            }
+
+        }
+
+        int ans = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] >= 2)
+                ans++;
+        }
+        bw.write(ans + "\n");
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
